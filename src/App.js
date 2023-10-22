7b75a532dcdbfe3f47d22d6bad47edbc75c9c5e2
Becca <rkinnee91@hotmail.com>
2023-10-22 16:50:26 -0700

    remove files and add basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App__title">Title</h1>
+      
+      <div className="App__locationContainer">
+        <h3 className="App__location">Home</h3>
+        <h3 className="App__location">About</h3>
+        <h3 className="App__location">Blog</h3>
+        <h3 className="App__location">Contact</h3>
+      </div>
     </div>
   );
 }
